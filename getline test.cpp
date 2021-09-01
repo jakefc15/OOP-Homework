39c89dc5043f533ae7f6edd764ab528929626d9e
jakefc15 <89606870+jakefc15@users.noreply.github.com>
2021-09-01 09:49:42 -0500

    Create getline test.cpp

---
+++ getline test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string str1, str2;
+
+    cout << "type Jake Gilbert (getline  test)" << endl << endl;
+   // getline(cin, str1);
+    cout << str1 << endl << endl;
+
+    cout << "type Jake Gilbert (cin test)" << endl << endl;
+    //cin >> str2;
+    cout << str2 << endl << endl;
+
+    return 0;
+}
