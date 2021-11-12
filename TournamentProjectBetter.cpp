790591cca2ef7a25d510cb1f8b0af5517d549177
jakefc15 <89606870+jakefc15@users.noreply.github.com>
2021-11-11 23:16:22 -0600

    Update TournamentProjectBetter.cpp

--- TournamentProjectBetter.cpp
+++ TournamentProjectBetter.cpp
@@ -5,20 +5,34 @@
 using namespace std;
 
 
+class Dates 
+{
+public:
+    string day1, day2;
+    
+    Dates()
+    {
+    day1 = "Friday";
+    day2 = "Saturday";
+        
+    }
+};
 
 class Team {
 public:
-    
     string name;
-      
+    
 };
 
 int main()
 {
-    Team team1, team2, team3, team4, team5, team6, team7, team8;
+    Dates dates;
     
-    cout << "Welcome to the Basketball Tournament!\nTo sign up...\n";
+    cout << "Welcome to the Basketball Tournament!\n";
+    cout<< "Tournament will be played on " << dates.day1 << " and " << dates.day2 << endl << "\nTo sign up...\n";
     cout << "Please input team names.\n\n";
+    
+    Team team1, team2, team3, team4, team5, team6, team7, team8;
     
     cin >> team1.name;
     cin >> team2.name;
@@ -28,6 +42,7 @@ int main()
     cin >> team6.name;
     cin >> team7.name;
     cin >> team8.name;
+    
 
     string teams[8];
     teams[0] = team1.name;
@@ -39,16 +54,15 @@ int main()
     teams[6] = team7.name;
     teams[7] = team8.name;
 
-    cout << "List of Teams:\n\n";
 
+    cout << "List of Teams:\n\n";
 
     for (int i = 0; i < 8; i++){
         cout << teams[i] << "\n";
     }
 
-    
     random_shuffle(&teams[0], &teams[7]);
-
+    
     
     cout << "\n\nFirst Round of Tournament:\n\n";
     cout << teams[0] << " VS. " << teams[7] << endl;
@@ -56,6 +70,7 @@ int main()
     cout << teams[2] << " VS. " << teams[5] << endl;
     cout << teams[3] << " VS. " << teams[4] << endl;
 
+    
     cout << "\n\nThank You for Signing up for the tournament.\n\nGood Luck!";
-
+        
 }
